c16305de135d82382f4def46bec4d7818d8cacf2
zarrar <zarrarmalik81@gmail.com>
2020-05-30 18:58:41 +0100

    Add test for owner class

---
+++ testing-java-junit5/src/test/java/com/malik/zarrar/petclinic/model/OwnerTest.java
@@ -0,0 +1,29 @@
+package com.malik.zarrar.petclinic.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OwnerTest {
+
+	
+	// Owner extends to person, so we need both set of properties
+	  @Test
+	    void dependentAssertions() {
+
+	        Owner owner = new Owner(1l, "Joe", "Buck");
+	        owner.setCity("Key West");
+	        owner.setTelephone("1231231234");
+
+	        // We have mulitple layers of assertAll 
+	        assertAll("Properties Test",
+	                () -> assertAll("Person Properties",
+	                        () -> assertEquals("Joe", owner.getFirstName(), "First Name Did not Match"),
+	                        () -> assertEquals("Buck", owner.getLastName())),
+	                () -> assertAll("Owner Properties",
+	                        () -> assertEquals("Key West", owner.getCity(), "City Did Not Match"),
+	                        () -> assertEquals("1231231234", owner.getTelephone())
+	                ));
+	    }
+
+}
