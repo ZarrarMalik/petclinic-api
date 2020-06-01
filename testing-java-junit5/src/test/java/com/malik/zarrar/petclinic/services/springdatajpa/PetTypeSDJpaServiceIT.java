0633dedacfccc2c34921867bdadfa9f1687ad76d
Zarrar Ali Malik <zarrarmalilk81@gmail.com>
2020-06-01 16:36:40 +0100

    Add an IT for petTypeservice

---
+++ testing-java-junit5/src/test/java/com/malik/zarrar/petclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -0,0 +1,46 @@
+package com.malik.zarrar.petclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.malik.zarrar.petclinic.extensions.TimingExtension;
+
+class PetTypeSDJpaServiceIT {
+
+	@ExtendWith(TimingExtension.class)
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testPetTypeSDJpaService() {
+
+	}
+
+	@Test
+	void testFindAll() {
+
+	}
+
+	@Test
+	void testFindById() {
+
+	}
+
+	@Test
+	void testSave() {
+
+	}
+
+	@Test
+	void testDelete() {
+
+	}
+
+	@Test
+	void testDeleteById() {
+
+	}
+
+}
