0f44de74fdf40e3015c15130461d2e4f9a038626
zarrar <zarrarmalik81@gmail.com>
2020-06-01 11:48:29 +0100

    Add arguments for repeated tests

--- testing-java-junit5/src/test/java/com/malik/zarrar/petclinic/model/PersonTest.java
+++ testing-java-junit5/src/test/java/com/malik/zarrar/petclinic/model/PersonTest.java
@@ -3,32 +3,34 @@ package com.malik.zarrar.petclinic.model;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class PersonTest {
 
 	Person person = new Person(2L, "Joe", "Black");
-	
+
 	@Test
 	void testGroupedAssertions() {
 		// Uses multiple asserts in a single test with lambdas
-		assertAll("Test Prop set", ()-> assertEquals("Joe", person.getFirstName()),
-				()->assertEquals("Black", person.getLastName()));
+		assertAll("Test Prop set", () -> assertEquals("Joe", person.getFirstName()),
+				() -> assertEquals("Black", person.getLastName()));
 	}
-	
+
 	@Test
 	void testGroupedAssertionsWithMessages() {
 		// Uses multiple asserts in a single test with lambdas
-		assertAll("Test Prop set", ()-> assertEquals("Joe", person.getFirstName(), "First Name failed"),
+		assertAll("Test Prop set", () -> assertEquals("Joe", person.getFirstName(), "First Name failed"),
 				() -> assertEquals("Black", person.getLastName(), "Second Name failed"));
 	}
-	
-	//Repeats test 10 times
-	@RepeatedTest(value = 10)
+
+	// Repeats test 10 times
+	@RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+	@DisplayName("My Repeated Test")
 	@Test
 	void testRepeatedTest() {
-		
+
 	}
 
 }
