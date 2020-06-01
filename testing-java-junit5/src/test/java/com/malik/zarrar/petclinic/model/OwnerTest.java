dfae82f69378646837d63e129793f9fe157a1ce3
Zarrar Ali Malik <zarrarmalilk81@gmail.com>
2020-06-01 14:56:09 +0100

    Add parameterirzed tests

--- testing-java-junit5/src/test/java/com/malik/zarrar/petclinic/model/OwnerTest.java
+++ testing-java-junit5/src/test/java/com/malik/zarrar/petclinic/model/OwnerTest.java
@@ -5,29 +5,37 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class OwnerTest {
 
-	
 	// Owner extends to person, so we need both set of properties
-	  @Test
-	    void dependentAssertions() {
+	@Test
+	void dependentAssertions() {
 
-	        Owner owner = new Owner(1l, "Joe", "Buck");
-	        owner.setCity("Key West");
-	        owner.setTelephone("1231231234");
+		Owner owner = new Owner(1l, "Joe", "Buck");
+		owner.setCity("Key West");
+		owner.setTelephone("1231231234");
 
-	        // We have mulitple layers of assertAll 
-	        assertAll("Properties Test",
-	                () -> assertAll("Person Properties",
-	                        () -> assertEquals("Joe", owner.getFirstName(), "First Name Did not Match"),
-	                        () -> assertEquals("Buck", owner.getLastName())),
-	                () -> assertAll("Owner Properties",
-	                        () -> assertEquals("Key West", owner.getCity(), "City Did Not Match"),
-	                        () -> assertEquals("1231231234", owner.getTelephone())
-	                ));
-	        //Hamcrest assertThat
-	        assertThat(owner.getCity(), is("Key West"));
-	    }
+		// We have mulitple layers of assertAll
+		assertAll("Properties Test",
+				() -> assertAll("Person Properties",
+						() -> assertEquals("Joe", owner.getFirstName(), "First Name Did not Match"),
+						() -> assertEquals("Buck", owner.getLastName())),
+				() -> assertAll("Owner Properties",
+						() -> assertEquals("Key West", owner.getCity(), "City Did Not Match"),
+						() -> assertEquals("1231231234", owner.getTelephone())));
+		// Hamcrest assertThat
+		assertThat(owner.getCity(), is("Key West"));
+	}
+
+	// params dependency
+	@ParameterizedTest
+	@ValueSource(strings = { "Spring", "Framework", "Guru" })
+	void testValueSource(String val) {
+		//Prints spring, framework and guru
+		System.out.println(val);
+	}
 
 }
