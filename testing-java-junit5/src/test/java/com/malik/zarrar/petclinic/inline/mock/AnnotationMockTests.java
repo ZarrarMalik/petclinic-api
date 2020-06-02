685d5a5b92796e5d5cc39fb8eecd836fc8c9da14
Zarrar Ali Malik <zarrarmalilk81@gmail.com>
2020-06-02 07:47:49 +0100

    Add mockito dependency and add tsts

---
+++ testing-java-junit5/src/test/java/com/malik/zarrar/petclinic/inline/mock/AnnotationMockTests.java
@@ -0,0 +1,23 @@
+package com.malik.zarrar.petclinic.inline.mock;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class AnnotationMockTests {
+	@Mock
+	Map<String, Object> mockMap;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	void testMock() {
+		mockMap.put("keyvalue", "foo");
+	}
+}
